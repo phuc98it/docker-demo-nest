80798de6c5d518db622f4deb7152ec3c78d74058
phuc98it <phucpham98.it.vn@gmail.com>
2024-10-11 18:48:00 +0700

    build and migration success in docker pro

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import Address from '../users/address.entity';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
         port: configService.get('POSTGRES_PORT'),
         username: configService.get('POSTGRES_USER'),
         password: configService.get('POSTGRES_PASSWORD'),
--- src/database/typeOrm.config.ts
+++ src/database/typeOrm.config.ts
@@ -8,6 +8,7 @@ const configService = new ConfigService();
  
 export default new DataSource({
   type: 'postgres',
+  host: configService.get('POSTGRES_HOST'),
   port: configService.get('POSTGRES_PORT'),
   username: configService.get('POSTGRES_USER'),
   password: configService.get('POSTGRES_PASSWORD'),
